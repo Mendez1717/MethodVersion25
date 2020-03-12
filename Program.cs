d6a8eb99e62c58c341e415db97a18ca9066d3d83
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-03-11 22:31:17 -0400

    Version 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,51 @@ namespace MethodVersion25
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, my name is Mikayla Anne Mendez and this is Version 25!");
+            
+            int Num1;
+            int Num2;
+            int result;
+            
+            Console.WriteLine("Enter the symbol of which operation you would like to use out of Addition(+), Subtraction(-), Multiplication(*), or Division(/):  ");
+            string operationsign;
+            Console.Write("You Typed:  ");
+            operationsign = Console.ReadLine();
 
+            // Ask user to enter value
+            Console.Write("Enter the first number:  ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask user to enter value
+            Console.Write("Enter the second number:  ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (operationsign == "+")
+            {
+                result = Num1 + Num2;
+                Console.WriteLine("The sum of the Addition Operation is: {0}", result);
+            }
+            else if (operationsign == "-")
+            {
+                result = Num1 - Num2;
+                Console.WriteLine("The result from the Subtraction Operation is:  {0}", result);
+            }
+            else if (operationsign == "*")
+            {
+                result = Num1 * Num2;
+                Console.WriteLine("The result from the Multiplication Operation is:  {0}", result);
+
+            }
+            else if (operationsign == "/")
+            {
+                if (Num2 != 0)
+                {
+
+                    result = Num1 / Num2;
+                    Console.WriteLine("The result from the Division Operation is:  {0}", result);
+                }
+                else
+                { Console.WriteLine("DIV/ZERO!"); }
+            }
         }
     }
 }
